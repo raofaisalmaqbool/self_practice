4c2df92217656b8010f580f97032adb68884db4f
raofaisalmaqbool <raofaisalmaqbool@gmail.com>
2022-03-18 18:13:51 +0500

    Create a Dynamic URL like product page of ecommerace website

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+  
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("welcome to home")
+
+def courses(request):
+    return HttpResponse("welcome to courses")
+
+def coursedetails(request, productid):
+    return HttpResponse(productid)
--- self_practice/urls.py
+++ self_practice/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('courses/', views.courses, name='courses'),
+    path('courses/<productid>', views.coursedetails, name='coursedetails'),
 ]
