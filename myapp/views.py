f656bb36e44ef92d7142e62db9f904d7e345e93f
raofaisalmaqbool <raofaisalmaqbool@gmail.com>
2022-03-22 07:55:04 +0500

    18 If Else Statement in Django Template(html pageg) coundition apply on list of numbers

--- myapp/views.py
+++ myapp/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render, redirect, HttpResponse
 
+
 # Create your views here.
 
 def index(request):
     data = {
         'heading0' : 'helow yahan djnago sy data html page pr ha raha ha',
         'clist': ['php', 'java', 'python', 'djanago'],
-        'students' : [
-            {'name':'ali', 'phone':'789475987'},
+        'students': [
+            {'name': 'ali', 'phone': '789475987'},
             {'name': 'ahmad', 'phone': '457475987'}
-        ]
+        ],
+        'numbers': [1, 2, 3, 4, 5, 6, 7, 8, 9]
     }
     return render(request, "index.html", data)
+
 
 def home(request):
     return HttpResponse("welcome to home")
 
+
 def courses(request):
     return HttpResponse("welcome to courses")
+
 
 def coursedetails(request, productid):
     return HttpResponse(productid)
