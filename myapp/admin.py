b5b07f7645c8a7a2dffd59917ee5c0e5fc4c4025
raofaisalmaqbool <raofaisalmaqbool@gmail.com>
2022-04-02 23:28:38 +0500

    34 make model of service and register it in admin show like a table in penal by making a class ServiceAdmin in admin.py and with using list_display

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from myapp.models import Service
+from myapp.models import *
+
 
 # Register your models here.
+
+class ServiceAdmin(admin.ModelAdmin):
+    # list_display is neccessory write same word as it is
+    list_display = ('service_icon', 'service_title', 'service_des')
+
+
+admin.site.register(Service, ServiceAdmin)
